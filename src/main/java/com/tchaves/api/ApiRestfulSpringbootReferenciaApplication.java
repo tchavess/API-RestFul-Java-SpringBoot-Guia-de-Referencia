0c730175112d43f10673df59422e02ce9aa323f0
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-14 00:59:25 -0300

    Gerando senha criptografadas com BCrypt

--- src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -1,17 +1,15 @@
 package com.tchaves.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.tchaves.api.util.SenhaUtils;
+
 @SpringBootApplication
 public class ApiRestfulSpringbootReferenciaApplication {
 	
-	@Value("${paginacao.qtd_por_pagina}")
-	private int qtdPorPagina;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
 	}
@@ -19,7 +17,13 @@ public class ApiRestfulSpringbootReferenciaApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(){
 		return args ->{
-			System.out.println("### Quantidade de elementos por pagina: " + this.qtdPorPagina);
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded );
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded );
+			
+			System.out.println("Senha valida: " + SenhaUtils.senhaValida("123456", senhaEncoded) );
 		};
 	}
 
