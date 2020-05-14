4d5278da99cdd55b81d22ae0eb130edf50429d1d
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-13 23:57:14 -0300

    Integração continua com Travis CI

--- src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -8,6 +8,9 @@ public class ApiRestfulSpringbootReferenciaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
+		System.out.println("Travis OK!!!");
 	}
+	
+	
 
 }
