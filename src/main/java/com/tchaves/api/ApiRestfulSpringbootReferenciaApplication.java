d6281aa2cbdc9a69ef931f6b24e9a6687b66b03d
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-13 23:34:13 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.tchaves.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringbootReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchaves.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringbootReferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
