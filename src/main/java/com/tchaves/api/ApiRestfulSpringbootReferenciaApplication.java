69b9b43c1c6fa6fbaddb56607d5325fdf07f5428
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-14 00:36:45 -0300

    Adicionando profiles ao projeto application-test.properties

--- src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -1,16 +1,26 @@
 package com.tchaves.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiRestfulSpringbootReferenciaApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
-		System.out.println("Travis OK!!!");
 	}
 	
-	
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args ->{
+			System.out.println("### Quantidade de elementos por pagina: " + this.qtdPorPagina);
+		};
+	}
 
 }
