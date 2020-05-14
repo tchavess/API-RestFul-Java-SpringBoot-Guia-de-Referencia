1f11423f79a8ceb43d6737132a37e0b444ca3e33
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-14 16:38:36 -0300

    Criando Serviços(Service)

--- src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -1,56 +1,29 @@
 package com.tchaves.api;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.tchaves.api.entities.Empresa;
-import com.tchaves.api.repositories.EmpresaRepository;
+import com.tchaves.api.services.ExemploService;
 
 @SpringBootApplication
 public class ApiRestfulSpringbootReferenciaApplication {
 	
 	@Autowired
-	private EmpresaRepository empresaRepository;
+	private ExemploService exemploService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
 	}
 	
-	/*@Bean
+	@Bean
 	public CommandLineRunner commandLineRunner(){
 		return args ->{
+			this.exemploService.testarService();
 			
-			Empresa empresa = new Empresa();
-			empresa.setRazaoSocial("Tchaves Dev");
-			empresa.setCnpj("11021317764");
-			
-			this.empresaRepository.save(empresa);
-			
-			List<Empresa> empresas = empresaRepository.findAll();
-			empresas.forEach(System.out::println);
-			
-			Optional<Empresa> empresaDb = empresaRepository.findById(1L);
-			System.out.println("Empresa por ID: " + empresaDb);
-			
-			
-			
-			empresa.setRazaoSocial("KAzALES TI");
-			this.empresaRepository.save(empresa);
-			
-			Empresa empresaCnpj = empresaRepository.findByCnpj("11021317764");
-			System.out.println("Empresa por CNPJ: " + empresaCnpj);
-			
-			this.empresaRepository.deleteById(1L);
-			
-			empresas = empresaRepository.findAll();
-			System.out.println("Empresas: " + empresas.size());
 		};
-	}*/
+	}
 
 }
---
+++ src/main/java/com/tchaves/api/services/ExemploService.java
@@ -0,0 +1,11 @@
+package com.tchaves.api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+
+	public void testarService(){
+		System.out.println("### Executando servico de teste!");
+	}
+}
