6a1868f39566d1aefd55d36d98619e8bcd9e4916
Thiago Chaves <thiago.chaves04@gmail.com>
2020-05-15 17:59:06 -0300

    Criando Controllers

--- src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
+++ src/main/java/com/tchaves/api/ApiRestfulSpringbootReferenciaApplication.java
@@ -5,25 +5,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.tchaves.api.services.ExemploService;
+
 
 @SpringBootApplication
 public class ApiRestfulSpringbootReferenciaApplication {
 	
-	@Autowired
-	private ExemploService exemploService;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestfulSpringbootReferenciaApplication.class, args);
 	}
 	
-	@Bean
+	/*@Bean
 	public CommandLineRunner commandLineRunner(){
 		return args ->{
-			this.exemploService.testarService();
+			
 			
 		};
-	}
+	}*/
 
 }
